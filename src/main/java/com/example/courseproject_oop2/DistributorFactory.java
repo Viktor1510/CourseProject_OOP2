45615174e1f6b7adc3b7fb5e65e2796f12bf7ba8
Viktor1510 <aleksandrov.ivanov02@gmail.com>
2023-10-25 15:46:32 +0300

    Adding Factory pattern

--- src/main/java/com/example/courseproject_oop2/DistributorFactory.java
+++ src/main/java/com/example/courseproject_oop2/DistributorFactory.java
@@ -3,6 +3,6 @@ package com.example.courseproject_oop2;
 public class DistributorFactory implements UserAbstractFactory{
     @Override
     public User createUser(String username, String password) {
-        return new Cashier(username,password,Role.CASHIER);
+        return new Cashier(username,password,Role.DISTRIBUTOR);
     }
 }
